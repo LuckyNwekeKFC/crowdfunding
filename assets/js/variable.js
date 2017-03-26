8d51fc8971f383a230740a0e76bcdf4f57aebbdc
Ola <innovator256@users.noreply.github.com>
2017-03-26 18:22:14 -0300

    Crowdfunding postponed
    Crowdfunding indicator postponed to deliver more value before distribution event

--- assets/js/variable.js
+++ assets/js/variable.js
@@ -35,7 +35,7 @@ var _animationDuration = 1750; // animation duration
 countdown
 ==============================================================================*/
 var _countdown = true; // countdown toggle
-var _countdownDate = '3/27/2017 23:59:59'; // 2015-12-24 23:59:59
+var _countdownDate = '6/12/2017 23:59:59'; // 2015-12-24 23:59:59
 var _countdownTimezone = '-8'; // timezone
 
 /*=================================================
