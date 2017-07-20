2a3b3690cdf7249a7a449dd49db0b1420d45a106
Ola <innovator256@users.noreply.github.com>
2017-07-20 13:12:50 -0300

    new timeline

--- assets/js/variable.js
+++ assets/js/variable.js
@@ -35,7 +35,7 @@ var _animationDuration = 1750; // animation duration
 countdown
 ==============================================================================*/
 var _countdown = true; // countdown toggle
-var _countdownDate = '9/42/2017 23:59:59'; // 2015-12-24 23:59:59
+var _countdownDate = '11/14/2017 23:59:59'; // 2015-12-24 23:59:59
 var _countdownTimezone = '-8'; // timezone
 
 /*=================================================
